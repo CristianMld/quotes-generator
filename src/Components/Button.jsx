a4f1de0ca1b148c4c6d6a893f3dd77c3ed805bbb
CristianMld <hkrcrm@gmail.com>
2022-10-22 00:28:31 -0300

    adding a third component

---
+++ src/Components/Button.jsx
@@ -0,0 +1,15 @@
+
+const Button = ({setIndex, quotesLength, color}) => {
+  const change = () => {
+    const secondRandom = Math.floor(Math.random() * quotesLength);
+    setIndex(secondRandom);
+  }
+
+  return (
+    <div>
+      <button style={{background: color}} onClick={change}><i className="fa-solid fa-shuffle"></i></button>      
+    </div>
+  );
+};
+
+export default Button;
--- src/Components/QuoteBox.jsx
+++ src/Components/QuoteBox.jsx
@@ -1,16 +1,18 @@
+import Button from "./Button";
 
 const QuoteBox = ({quote, author, setIndex, quotesLength, color}) => {
-  const change = () => {
-    const secondRandom = Math.floor(Math.random() * quotesLength);
-    setIndex(secondRandom);
-  }
+  
 
   return (
     <div className="card">
       <h3><i className="fa-sharp fa-solid fa-quote-left"></i> {quote} {" "} 
       <i className="fa-sharp fa-solid fa-quote-right"></i></h3>
       <h4>- {author}</h4>
-      <button style={{background: color}} onClick={change}><i className="fa-solid fa-shuffle"></i></button>
+      <Button 
+      setIndex={setIndex}
+      quotesLength={quotesLength}
+      color={color}
+      />
     </div>
   );
 };
